88a0cea3f7173b920b00073e3a6f739fe77475d2
Morgane-Rabiller <90465154+Morgane-Rabiller@users.noreply.github.com>
2023-12-12 19:49:01 +0100

    first commit

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,12 @@
 // == Import
-import reactLogo from './react-logo.svg';
-import './styles.css';
+import Header from 'src/components/Header';
+import './styles.scss';
 
 // == Composant
 const App = () => (
   <div className="app">
-    <img src={reactLogo} alt="react logo" />
-    <h1>Composant : App</h1>
+    <h1 className='app__title'>MOGO</h1>
+    <Header />
   </div>
 );
 
---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import './styles.scss';
+
+const Header = () => (
+  <div className="header">
+    <a href="/" className="header__link">Présentation</a>
+    <a href="/" className="header__link">parcours</a>
+    <a href="/" className="header__link">compétences</a>
+    <a href="/" className="header__link">projets</a>
+    <a href="/" className="header__link">contact</a>
+  </div>
+);
+
+export default Header;
