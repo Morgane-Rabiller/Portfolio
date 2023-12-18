07a0cef794750e980d127a32042546e05975694f
Morgane-Rabiller <90465154+Morgane-Rabiller@users.noreply.github.com>
2023-12-18 16:05:25 +0100

    added css for appear projects

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,34 +1,72 @@
+import { useEffect, useRef } from "react";
 import "./styles.scss";
 import apet4life from "src/assets/img/projects/apet4life.png";
 import omyband from "src/assets/img/projects/omyband.png";
+import Typewriter from "typewriter-effect";
 
-const Projects = () => (
-    <div className="projects">
-        <p className="projects__title">MES PROJETS</p>
-        <p>Durant de mes deux formations j'ai pratiqué les notions appris dans divers projets proposés par la formation absolument tous les jours via différents challenges. Cependant, les principaux projets où on a pu mettre en pratique tout ce qu'on avait appris tout au long de l'année sont ceux que je présente ici.</p>
-        <div className="projects__list">
-            <div className="project">
-                <p className="project__title">A Pet 4 Life</p>
-                <img className="project__img" src={apet4life} alt="aPet4Life" />
-                <p className="project__desc">Projet réalisé en fin de formation DWWM</p>
-                <a
-                    className="project__link"
-                    href="https://apet4life.huiitre.fr/"
-                    target="_blanc"
-                >
-                    Lien vers le site
-                </a>
-            </div>
-            <div className="project">
-                <p className="project__title">O'MY BAND</p>
-                <img className="project__img" src={omyband} alt="omyband" />
-                <p className="project__desc">Projet réalisé en fin de formation CDA</p>
-                <a className="project__link" href="" target="_blanc">
-                    Lien à venir
-                </a>
+const Projects = () => {
+  const refProject1 = useRef();
+  const refProject2 = useRef();
+
+  useEffect(() => {
+    window.setTimeout(() => {
+      refProject1.current.style.transition = "5s";
+      refProject1.current.style.opacity = 1;
+      refProject2.current.style.transition = "5s";
+      refProject2.current.style.opacity = 1;
+    }, 3000)
+  }, []);
+
+    return (
+        <div className="projects">
+            <p className="projects__title">MES PROJETS</p>
+
+            <Typewriter
+                onInit={(typewriter) => {
+                    typewriter
+                        .typeString(
+                            "Durant de mes deux formations j'ai pratiqué les notions appris dans divers projets proposés par la formation absolument tous les jours via différents challenges. Cependant, les principaux projets où on a pu mettre en pratique tout ce qu'on avait appris tout au long de l'année sont ceux que je présente ici."
+                        )
+                        .start();
+                }}
+                options={{
+                    autoStart: true,
+                    delay: 10,
+                    wrapperClassName: ".description__content",
+                }}
+            />
+            <div className="projects__list">
+                <div className="project" ref={refProject1}>
+                    <p className="project__title">A Pet 4 Life</p>
+                    <img
+                        className="project__img"
+                        src={apet4life}
+                        alt="aPet4Life"
+                    />
+                    <p className="project__desc">
+                        Projet réalisé en fin de formation DWWM
+                    </p>
+                    <a
+                        className="project__link"
+                        href="https://apet4life.huiitre.fr/"
+                        target="_blanc"
+                    >
+                        Lien vers le site
+                    </a>
+                </div>
+                <div className="project" ref={refProject2}>
+                    <p className="project__title">O'MY BAND</p>
+                    <img className="project__img" src={omyband} alt="omyband" />
+                    <p className="project__desc">
+                        Projet réalisé en fin de formation CDA
+                    </p>
+                    <a className="project__link" href="" target="_blanc">
+                        Lien à venir
+                    </a>
+                </div>
             </div>
         </div>
-    </div>
-);
+    );
+};
 
 export default Projects;
