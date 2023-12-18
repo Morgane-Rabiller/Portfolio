708c35e1269ea41a40a7609f71413a2906365b49
Morgane-Rabiller <90465154+Morgane-Rabiller@users.noreply.github.com>
2023-12-18 17:08:04 +0100

    update description

--- src/data/description.js
+++ src/data/description.js
@@ -8,12 +8,12 @@ export default {
   Titulaire d'un titre professionnel "développeur web et web mobile" équivalent bac+2 acquis grâce à l'école O'clock, je suis maintenant sur la fin de mon alternance visant à acquérir le titre professionnel "concepteur développeur d'applications" équivalent bac+3/4 qui se termine le 2 Février 2024 toujours avec l'école O'clock. Je passerais mon examen le 31 Janvier 2024.<br> <br>
 
   Durant ma première formation, j'ai pu apprendre les bases de la programmation, HTML, CSS, PHP, Javascript, la programmation orientée objet, l'architecture MVC, le SGBD mysql, l'API REST, la sécurité contre les injections SQL, les failles XSS et les failles CSRF et je me suis spécialisé sur le framework Symfony avec l'ORM Doctrine, le système d'authentification avec le token JWT et les tests unitaires avec PHPunit.<br>
-  Durant ma seconde formation j'ai pu étudier la conception (diagramme de séquence, de classe, de cas d'utilisation, entité-association), le framework laravel, le framework VueJS, l'environnement nodeJS (serveur express), l'ORM Sequelize, l'API graphQL (avec le serveur appolo), mais aussi du noSQL (mongoDB, redis), le SGBD PostgreSQL, j'ai également revue la sécurité (ANSSI, CNIL, OWASP, ...), le langage dart et le framework Flutter pour le développement d'applications.<br> <br>
+  Durant ma seconde formation j'ai pu étudier la conception (diagramme de séquence, de classe, de cas d'utilisation, entité-association), le framework laravel, le framework VueJS, la librairie React, l'environnement nodeJS (serveur express), l'ORM Sequelize, l'API graphQL (avec le serveur appolo), mais aussi du noSQL (mongoDB, redis), le SGBD PostgreSQL, les tests unitaires avec Jest, j'ai également revue la sécurité (ANSSI, CNIL, OWASP, ...), le langage dart et le framework Flutter pour le développement d'applications.<br> <br>
 
   Je suis désormais en recherche d’une entreprise qui voudra bien m’accueillir en CDI pour mon premier emploi sur un poste de développeuse REACT, VueJS et/ou Symfony. <br> <br>
 
   Et sinon, hors du cadre du travail ?  <br> 
-  Je suis plutôt casanière du genre à bien aimer ma routine et chill devant mes films et mes séries mais j'aime faire la fête, j'aime les balades en forêt et en montagnes, j'aime voyager et découvrir de nouvelles cultures. <br>
-  Ma plus grande passion c'est la musique, j'adore chanter ! J'ai une guitare mais je n'y joue plus vraiment et j'aime bien aussi la batterie. <br>
-  Mais ce que j'aime par dessus tout c'est mon amour de chat 'Mimi' 💙`,
+  Je suis plutôt casanière du genre à bien aimer ma routine et chill devant mes films et mes séries mais j'aime faire la fête, j'aime les balades en forêt et en montagnes, j'ai pu découvrir le plaisir de la spéléologie et de la via ferrata il y a peu, j'aime voyager et découvrir de nouvelles cultures. <br>
+  Ma plus grande passion c'est la musique, j'écoute de tout mais principalement du rock / métal et j'adore chanter ! 🎤<br>
+  De plus, vous l'aurez compris via mon logo, je suis team chat 💙`,
 }
