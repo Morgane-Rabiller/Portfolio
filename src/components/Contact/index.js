bb5e9588baa2eb9c086d94fc6368d85945757577
Morgane-Rabiller <90465154+Morgane-Rabiller@users.noreply.github.com>
2023-12-19 08:37:42 +0100

    change css in contact page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -6,28 +6,28 @@ import linkedin from 'src/assets/img/contact/linkedin.png';
 import github from 'src/assets/img/contact/github.png';
 
 const Contact = () => (
-  <div className="contact">
-    <p className="contact__title">ME CONTACTER</p>
-    <div className='contact__coords'>
-      <div className='contact__coord'>
-        <img className='contact__coord--img' src={tel} alt="phone" />
+  <div className="contact my-6 mx-auto p-5 text-center w-12 md:w-9 lg:w-6 xl:w-4">
+    <p className="font-bold text-2xl text-center mb-5">ME CONTACTER</p>
+    <div>
+      <div className='flex align-items-center justify-content-start mb-3'>
+        <img className='w-3rem mr-3' src={tel} alt="phone" />
         <p> 07 60 75 02 87</p>
       </div>
-      <div className='contact__coord'>
-        <img className='contact__coord--img' src={adress} alt="adress" />
+      <div className='flex align-items-center justify-content-start mb-3'>
+        <img className='w-3rem mr-3' src={adress} alt="adress" />
         <p>Couëron</p>
       </div>
-      <div className='contact__coord'>
-        <img className='contact__coord--img' src={mail} alt="mail" />
+      <div className='flex align-items-center justify-content-start mb-3'>
+        <img className='w-3rem mr-3' src={mail} alt="mail" />
         <p>morganerabiller@yahoo.fr</p>
       </div>
-      <div className='contact__coord'>
-        <img className='contact__coord--img' src={linkedin} alt="linkedin" />
-        <a className='contact__coord--link' href='https://www.linkedin.com/in/morgane-rabiller-996a28225/'>Morgane Rabiller</a>
+      <div className='flex align-items-center justify-content-start mb-3'>
+        <img className='w-3rem mr-3' src={linkedin} alt="linkedin" />
+        <a className='hover:underline' href='https://www.linkedin.com/in/morgane-rabiller-996a28225/'>Morgane Rabiller</a>
       </div>
-      <div className='contact__coord'>
-        <img className='contact__coord--img' src={github} alt="github" />
-        <a className='contact__coord--link' href='https://github.com/Morgane-Rabiller'>Morgane-Rabiller</a>
+      <div className='flex align-items-center justify-content-start mb-3'>
+        <img className='w-3rem mr-3' src={github} alt="github" />
+        <a className='hover:underline' href='https://github.com/Morgane-Rabiller'>Morgane-Rabiller</a>
       </div>
     </div>
   </div>
