ff6b65e2cb3c19eeea17d33c88349a0642a660c9
Morgane-Rabiller <90465154+Morgane-Rabiller@users.noreply.github.com>
2023-12-19 12:06:44 +0100

    update css in stack component and reworking career text

--- src/components/Skills/stack.js
+++ src/components/Skills/stack.js
@@ -1,7 +1,7 @@
 import "./styles.scss";
 
 const Stack = ({ img, content }) => (
-    <div className="m-2">
+    <div className="m-2 flex flex-column align-items-center">
         <img className="w-3rem" src={img} alt={content} />
         <p>{content}</p>
     </div>
