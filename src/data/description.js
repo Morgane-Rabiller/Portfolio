58a9c4a05e2b15680745897fffe4795e1ef4d906
Morgane-Rabiller <90465154+Morgane-Rabiller@users.noreply.github.com>
2024-02-06 14:29:28 +0100

    updated description

--- src/data/description.js
+++ src/data/description.js
@@ -3,14 +3,14 @@ export default {
   content: `
   Bonjour et bienvenue dans mon <strong>portfolio</strong>, je vais tout d'abord commencer par me présenter, et ensuite, je vous laisserais explorer le reste de mon site 😊 <br> <br>
 
-  Je m'appelle <strong>Morgane Rabiller</strong>, j'ai 27 ans, je suis développeuse web et je vais bientôt terminer une formation pour devenir conceptrice développeuse d'applications. <br> <br>
+  Je m'appelle <strong>Morgane Rabiller</strong>, j'ai 27 ans, je suis développeuse web et je viens de terminer une formation de conceptrice développeuse d'applications. <br> <br>
 
-  Titulaire d'un titre professionnel "développeur web et web mobile" équivalent bac+2 acquis grâce à l'école O'clock, je suis maintenant sur la fin de mon alternance visant à acquérir le titre professionnel "concepteur développeur d'applications" équivalent bac+3/4 qui se termine le 2 Février 2024 toujours avec l'école O'clock. Je passerais mon examen le 31 Janvier 2024.<br> <br>
+  Titulaire d'un titre professionnel "développeur web et web mobile" équivalent bac+2 acquis grâce à l'école O'clock, j'ai maintenant terminé mon alternance visant à acquérir le titre professionnel "concepteur développeur d'applications" équivalent bac+3/4 qui s'est terminé le 2 Février 2024 toujours avec l'école O'clock. J'ai passé mon examen le 31 Janvier 2024 et je suis maintenant dans l'attente des résultats.<br> <br>
 
   Durant ma première formation, j'ai pu apprendre les bases de la programmation, HTML, CSS, PHP, Javascript, la programmation orientée objet, l'architecture MVC, le SGBD mysql, l'API REST, la sécurité contre les injections SQL, les failles XSS et les failles CSRF et je me suis spécialisé sur le framework Symfony avec l'ORM Doctrine, le système d'authentification avec le token JWT et les tests unitaires avec PHPunit.<br>
   Durant ma seconde formation j'ai pu étudier la conception (diagramme de séquence, de classe, de cas d'utilisation, entité-association), le framework laravel, le framework VueJS, la librairie React, l'environnement nodeJS (serveur express), l'ORM Sequelize, l'API graphQL (avec le serveur appolo), mais aussi du noSQL (mongoDB, redis), le SGBD PostgreSQL, les tests unitaires avec Jest, j'ai également revue la sécurité (ANSSI, CNIL, OWASP, ...), le langage dart et le framework Flutter pour le développement d'applications.<br> <br>
 
-  Je suis désormais en recherche d’une entreprise qui voudra bien m’accueillir en CDI pour mon premier emploi sur un poste de développeuse REACT, VueJS et/ou Symfony. <br> <br>
+  Je suis désormais en recherche d’une entreprise qui voudra bien m’accueillir en CDI pour mon premier emploi sur un poste de développeuse REACT, VueJS et/ou NodeJS mais je suis également ouverte à d'autres technologies que j'ai pu voir et à en découvrir de nouvelles. <br> <br>
 
   Et sinon, hors du cadre du travail ?  <br> 
   Je suis plutôt casanière du genre à bien aimer ma routine et chill devant mes films et mes séries mais j'aime faire la fête, j'aime les balades en forêt et en montagnes, j'ai pu découvrir le plaisir de la spéléologie et de la via ferrata il y a peu, j'aime voyager et découvrir de nouvelles cultures. <br>
