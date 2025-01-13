afc83de473bca40c635d134e970e0b6cd1b66840
Morgane-Rabiller <90465154+Morgane-Rabiller@users.noreply.github.com>
2025-01-13 15:13:56 +0100

    added change presentation section

--- src/components/Description/index.js
+++ src/components/Description/index.js
@@ -2,30 +2,17 @@ import "./styles.scss";
 import description from "src/data/description";
 import DOMPurify from "dompurify";
 import Typewriter from "typewriter-effect";
-import { useState, useRef } from "react";
 
 const Description = () => {
-    const typewriterRef = useRef(null);
-    const [isCompleted, setIsCompleted] = useState(false);
     const cleanContent = DOMPurify.sanitize(description.content, {
         ALLOWED_TAGS: ["br", "strong"],
     });
 
-    const showFullText = () => {
-      setIsCompleted(true);  // Marque l'état comme "complété"
-      typewriterRef.current?.stop();  // Stoppe le défilement de texte
-    };
-
     return (
-        <div className="description p-5 my-5 md:mx-8">
-          <div className="flex align-items-center align-content-center">
-            <p className="description__title font-bold text-2xl text-center mb-5 flex-1">{description.title}</p>
-            <button onClick={showFullText} className="px-4 py-2 bg-blue-700 text-white rounded ml-auto"> Arreter le défilement </button>
-          </div>
-            {isCompleted ? <p className="font-semibold" dangerouslySetInnerHTML={{ __html: cleanContent }} /> : (
-              <Typewriter
+        <div className="text-center p-5 my-5 md:mx-8">
+            <p className="description__title font-bold text-2xl text-center mb-5">{description.title}</p>
+            <Typewriter
                 onInit={(typewriter) => {
-                    typewriterRef.current = typewriter;
                     typewriter
                         .typeString(cleanContent)
                         .start();
@@ -33,11 +20,9 @@ const Description = () => {
                 options={{
                     autoStart: true,
                     delay: 10,
-                    wrapperClassName: "font-semibold",
+                    wrapperClassName: "font-semibold description__text",
                 }}
-              />
-            )}
-            
+            />
             <div>
               
             </div>
@@ -46,3 +31,4 @@ const Description = () => {
 };
 
 export default Description;
+
--- src/data/description.js
+++ src/data/description.js
@@ -1,19 +1,9 @@
 export default {
-  title: "MA PRESENTATION",
+  title: "MOGO.CODE",
   content: `
-  Nom: Morgane rabiller <br/><br/>
-  Métier : Développeuse web Fullstack<br/><br/><br/>
-  Bonjour et bienvenue dans mon <strong>portfolio</strong>, je vais tout d'abord commencer par me présenter, et ensuite, je vous laisserais explorer le reste de mon site. 😊 <br> <br>
-
-  Je suis titulaire d'un titre professionnel "développeur web et web mobile" équivalent bac+2 et d'un titre professionnel "concepteur développeur d'applications" équivalent bac+3/4 réalisé en alternance avec l'école O'clock, je suis maintenant une développeuse épanouïe. 🎉<br> <br>
-
-  Durant ma première formation, j'ai pu apprendre les bases de la programmation, HTML, CSS, PHP, Javascript, la programmation orientée objet, l'architecture MVC, le SGBD mysql, l'API REST, la sécurité contre les injections SQL, les failles XSS et les failles CSRF et je me suis spécialisé sur le framework Symfony avec l'ORM Doctrine, le système d'authentification avec le token JWT et les tests unitaires avec PHPunit.<br>
-  Durant ma seconde formation j'ai pu étudier la conception (diagramme de séquence, de classe, de cas d'utilisation, entité-association), le framework laravel, le framework VueJS, la librairie React, l'environnement nodeJS (serveur express), l'ORM Sequelize, l'API graphQL (avec le serveur appolo), mais aussi du noSQL (mongoDB, redis), le SGBD PostgreSQL, les tests unitaires avec Jest, j'ai également revue la sécurité (ANSSI, CNIL, OWASP, ...), le langage dart et le framework Flutter pour le développement d'applications.<br> <br>
-
-  Je suis désormais dans une phase de lancement de mon entreprise en freelance. <br> <br>
-
-  Et sinon, hors du cadre du travail ?  <br> 
-  Je suis plutôt casanière du genre à bien aimer ma routine et chill devant mes films et mes séries mais j'aime faire la fête, j'aime les balades en forêt et en montagnes, j'ai pu découvrir le plaisir de la spéléologie et de la via ferrata il y a peu, j'aime voyager et découvrir de nouvelles cultures. <br>
-  Ma plus grande passion c'est la musique, j'écoute de tout mais principalement du rock / métal et j'adore chanter ! 🎤<br>
-  De plus, vous l'aurez compris via mon logo, je suis team chat ! 💙`,
+  Morgane rabiller <br/><br/>
+  Développeuse web Fullstack Javascript | React / Vue / Node <br/><br/><br/>
+  Vous avez besoin d'un site internet ? <br/> <br/>
+  Alors vous êtes bien tombé ! <br/> <br/>
+  Je m'occupe de la conception, du développement et de la livraison de votre site web et mobile. 😉`
 }
