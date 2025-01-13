ad576ea0385529b83552c7916ce6782e95e0be35
Morgane-Rabiller <90465154+Morgane-Rabiller@users.noreply.github.com>
2025-01-13 16:26:11 +0100

    added update in skills page

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -25,13 +25,13 @@ import scrum from "src/assets/img/skills/scrum.png";
 import Typewriter from "typewriter-effect";
 
 const Skills = () => (
-    <div className="skills p-5 my-5 md:mx-8">
-        <p className="font-bold text-2xl text-center mb-5">MES COMPÉTENCES</p>
+    <div className="p-5 my-5 md:mx-8 text-center">
+        <p className="skills__title font-bold text-2xl text-center mb-6">MES COMPÉTENCES</p>
         <Typewriter
             onInit={(typewriter) => {
                 typewriter
                     .typeString(
-                        "Voilà la liste des compétences que j'ai pu apprendre durant mes différentes formations :"
+                        "Voilà la liste des compétences essentielles que je maîtrise :"
                     )
                     .start();
             }}
@@ -41,29 +41,45 @@ const Skills = () => (
                 wrapperClassName: "font-semibold",
             }}
         />
-        <div className="flex flex-wrap gap-6 mt-5">
-            <Stack img={html} content="HTML5"/>
-            <Stack img={css} content="CSS3" />
-            <Stack img={javscript} content="Javascript" />
-            <Stack img={php} content="PHP" />
-            <Stack img={vue} content="VueJS" />
+        <div className="container flex flex-wrap justify-content-between mt-5">
+          <div className="languages mb-5">
+            <h1 className="text-xl mb-2">Langages</h1>
+            <div className="flex">
+              <Stack img={html} content="HTML5"/>
+              <Stack img={css} content="CSS3" />
+              <Stack img={javscript} content="Javascript" />
+              <Stack img={php} content="PHP" />
+            </div>
+          </div>
+          <div className="frameworks mb-5">
+            <h1 className="text-xl mb-2">Frameworks / Bibliothèques</h1>
+            <div className="flex">
             <Stack img={react} content="REACT" />
-            <Stack img={laravel} content="Laravel" />
-            <Stack img={symfony} content="Symfony" />
-            <Stack img={nodejs} content="NodeJS" />
-            <Stack img={mysql} content="MySQL" />
-            <Stack img={postgresql} content="PostgreSQL" />
-            <Stack img={mongodb} content="MongoDB" />
-            <Stack img={redis} content="Redis" />
-            <Stack img={dart} content="Dart" />
-            <Stack img={flutter} content="Flutter" />
-            <Stack img={graphql} content="GraphQL" />
-            <Stack img={git} content="Git" />
-            <Stack img={github} content="Github" />
-            <Stack img={figma} content="Figma" />
-            <Stack img={docker} content="Docker" />
-            <Stack img={trello} content="Trello" />
-            <Stack img={scrum} content="Méthode agile SCRUM" />
+              <Stack img={vue} content="VueJS" />
+              <Stack img={nodejs} content="NodeJS" />
+              <Stack img={symfony} content="Symfony" />
+            </div>
+          </div>
+          <div className="database mb-5">
+            <h1 className="text-xl mb-2">Base de données</h1>
+            <div className="flex">
+              <Stack img={mysql} content="MySQL" />
+              <Stack img={postgresql} content="PostgreSQL" />
+              <Stack img={graphql} content="GraphQL" />
+            </div>
+          </div>
+          <div className="tools mb-5">
+            <h1 className="text-xl mb-2">Outils et autre</h1>
+            <div className="flex">
+              <Stack img={git} content="Git" />
+              <Stack img={github} content="Github" />
+              <Stack img={figma} content="Figma" />
+              <Stack img={trello} content="Trello" />
+              <Stack img={scrum} content="Méthode agile SCRUM" />
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-wrap gap-6 mt-5">
         </div>
     </div>
 );
